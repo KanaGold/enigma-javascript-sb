37f81a1eecef56cc2f641c9c527fdbdcd41ff967
kanagold <michaelanderson0823@gmail.com>
2020-02-06 08:12:03 +0000

    init

--- src/utils/getEnigmaInit.js
+++ src/utils/getEnigmaInit.js
@@ -5,14 +5,18 @@ export default async () => {
     try {
         // Get network provider and web3 instance.
         const web3 = await getWeb3();
-        const EnigmaContract = require('../build/enigma_contracts/EnigmaSimulation.json');
+        // About EnimgaContract Object
+        // If Discovery is Running in Software simulation mode, use "EnigmaSimulation.json"
+        // If Discovery is Running in Hardware mode, use "Enigma.json"
+        //const EnigmaContract = require('../build/enigma_contracts/EnigmaSimulation.json');
+        const EnigmaContract = require('../build/enigma_contracts/Enigma.json');
         const EnigmaTokenContract = require('../build/enigma_contracts/EnigmaToken.json');
         const enigma = new Enigma(
             web3,
             EnigmaContract.networks['4447'].address,
             EnigmaTokenContract.networks['4447'].address,
             //'http://localhost:3333',
-            'http://51.145.139.98:3343',
+            'http://51.145.139.98:3333',
             {
                 gas: 4712388,
                 gasPrice: 100000000000,
